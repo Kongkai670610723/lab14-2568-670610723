2f5a4c62d7f0f0b2370ab53a668c5af6ba7249ff
Kong670610723 <Phurin0946122606@gmail.com>
2025-09-16 11:01:09 +0700

    lab14

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,10 +38,10 @@ export default function Sidebar() {
             color="red"
             withBorder
           >
-            <Avatar size="md" radius="xl" src="/charliebrown.jpg" />
+            <Avatar size="md" radius="xl" src="/picture.jpg" />
           </Indicator>
           <Text component={RouterNavLink} to="/">
-            User : Chanadda : Admin
+            User : Phurin : Student
           </Text>
         </Group>
       </Box>
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -24,8 +24,8 @@ export default function MainLayout() {
       <AppShell.Footer>
         <FooterComponent
           year="2025"
-          fullName="chanadda"
-          studentId="670612039"
+          fullName="Phurin Inthajak"
+          studentId="670610723"
         />
       </AppShell.Footer>
 
